f287d2c3e34dce24604f1e92ce30ce919e1b8a57
net <lbbpramuka@gmail.com>
2025-07-13 18:42:50 +0700

    fix(pagination): ensure correct data fetch when page changes

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,64 +1,40 @@
-import React, { useState } from "react";
+/* eslint-disable no-unused-vars */
+import React from "react";
 import Image from "next/image";
 import { useMediaQuery } from "@uidotdev/usehooks";
 
 type PaginationProps = {
   totalPosts?: number;
   postsPerPage: number;
-  // eslint-disable-next-line no-unused-vars
+  currentPage: number;
   onPageChange?: (pageNumber: number) => void;
 };
 
 const Pagination: React.FC<PaginationProps> = ({
   totalPosts,
   postsPerPage,
+  currentPage,
   onPageChange,
 }) => {
-  const [currentPage, setCurrentPage] = useState(1);
   const isMobile = useMediaQuery("only screen and (max-width : 1024px)");
   const totalPages = totalPosts ? Math.ceil(totalPosts / postsPerPage) : 0;
 
   const handleButtonClick = (pageNumber: number) => {
-    if (pageNumber > 0 && pageNumber <= totalPages) {
-      setCurrentPage(pageNumber);
-      if (onPageChange) {
-        onPageChange(pageNumber);
-      }
+    if (pageNumber > 0 && pageNumber <= totalPages && onPageChange) {
+      onPageChange(pageNumber);
     }
   };
 
   const getDisplayedPages = () => {
-    if (totalPages <= 7) {
-      return Array.from({ length: totalPages }, (_, i) => i + 1);
-    }
-
-    const pages = [];
+    if (totalPages <= 7) return Array.from({ length: totalPages }, (_, i) => i + 1);
 
     if (currentPage <= 4) {
-      pages.push(1, 2, 3, 4, 5, "...", totalPages);
+      return [1, 2, 3, 4, 5, "...", totalPages];
     } else if (currentPage >= totalPages - 3) {
-      pages.push(
-        1,
-        "...",
-        totalPages - 4,
-        totalPages - 3,
-        totalPages - 2,
-        totalPages - 1,
-        totalPages
-      );
+      return [1, "...", totalPages - 4, totalPages - 3, totalPages - 2, totalPages - 1, totalPages];
     } else {
-      pages.push(
-        1,
-        "...",
-        currentPage - 1,
-        currentPage,
-        currentPage + 1,
-        "...",
-        totalPages
-      );
+      return [1, "...", currentPage - 1, currentPage, currentPage + 1, "...", totalPages];
     }
-
-    return pages;
   };
 
   return (
