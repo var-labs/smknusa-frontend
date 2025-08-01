237a939bb612bd7de746f942f9b88418bdaebb94
net <lbbpramuka@gmail.com>
2025-08-01 16:23:52 +0700

    restyle: change home entrepenur video to principal's speech

--- src/components/home/home-entrepreneur-slider.tsx
+++ src/components/home/home-entrepreneur-slider.tsx
@@ -37,25 +37,26 @@ const HomeEntrepreneurSlider = () => {
   const { logos } = useLogos();
 
   return (
-    <div className="text-blue-base w-full flex max-w-max-content  justify-center  relative items-center  rounded-md  py-6 ">
-      <div className="absolute left-0 z-10 mix-blend-darken bg-gradient-to-r from-[#081B34] to-transparent py-20 px-4"></div>
-
-      <Marquee autoFill={true} speed={60}>
-        {logos?.map((data, index) => (
-          <React.Fragment key={index}>
-            <Image
-              src={backendUrl + data?.logo_mitra}
-              alt={data.nama_mitra}
-              width={data.width}
-              height={data.height}
-              style={{height: `${data.height}px`}}
-              className="xl:w-32 w-20 p-2 xl:p-3"
-            />
-          </React.Fragment>
-        ))}
-      </Marquee>
-      <div className="absolute right-0 z-10 mix-blend-darken bg-gradient-to-l from-[#081B34] to-transparent py-20 px-4"></div>
-    </div>
+    <>
+      {logos && logos.length > 0 && (<div className="text-blue-base w-full flex max-w-max-content  justify-center  relative items-center  rounded-md  py-6 ">
+        <div className="absolute left-0 z-10 mix-blend-darken bg-gradient-to-r from-[#081B34] to-transparent py-20 px-4"></div>
+          <Marquee autoFill={true} speed={60}>
+            {logos?.map((data, index) => (
+              <React.Fragment key={index}>
+                <Image
+                  src={backendUrl + data?.logo_mitra}
+                  alt={data.nama_mitra}
+                  width={data.width}
+                  height={data.height}
+                  style={{height: `${data.height}px`}}
+                  className="xl:w-32 w-20 p-2 xl:p-3"
+                />
+              </React.Fragment>
+            ))}
+          </Marquee>
+        <div className="absolute right-0 z-10 mix-blend-darken bg-gradient-to-l from-[#081B34] to-transparent py-20 px-4"></div>
+      </div>)}
+    </>
   );
 };
 
